9eea6738bd238ec563a373c608911a0159e109fb
BryceHuddleston <bryce.huddleston.5@gmail.com>
2022-01-10 14:43:14 -0600

    I added to the find_wind method and added more tests

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 import re
 
+wind_direction = None
+wind_speed = None
+gust_speed = None
+wind_unit = None
+
 
 def find_icao_in_observation(observation):
     if re.match("(METAR|SPECI|LWIS)?( COR)? ?([A-Z]{4} )", observation):
@@ -23,7 +28,20 @@ def find_modifier_in_observation(observation):
 
 
 def find_wind_in_observation(observation):
+    global wind_direction
+    global wind_speed
+    global gust_speed
+    global wind_unit
+
     x = re.match(
-        "(METAR|SPECI|LWIS)?( COR)? ?([A-Z]{4} )(\d+)Z (AUTO)? (.+)(KT|MPS)", observation)
-    wind_section = x.group(6) + x.group(7)
-    return wind_section
+        "(METAR|SPECI|LWIS)?( COR)? ?([A-Z]{4} )(\d+)Z (AUTO )?(.+)(KT|MPS)", observation)
+    wind_section = x.group(6)
+
+    wind_direction = wind_section[:3]
+    wind_speed = wind_section[3:5]
+    wind_unit = x.group(7)
+
+    if "G" in wind_section:
+        gust_speed = wind_section[6:8]
+    else:
+        gust_speed = "None"
--- tests/test_find_wind_main.py
+++ tests/test_find_wind_main.py
@@ -1,11 +1,64 @@
 import src.main as main
 
 
-def test_pass_in_observation_with_34013G16KT_in_it():
-    assert main.find_wind_in_observation(
-        "METAR KGNT 121815Z AUTO 34013G16KT 10SM CLR 12/M04 A3043 RMK AO2 T01231040=") == "34013G16KT"
+def test_find_wind_direction_in_observation():
+    main.find_wind_in_observation(
+        "METAR KGNT 121815Z AUTO 34013G16KT 10SM CLR 12/M04 A3043 RMK AO2 T01231040=")
+
+    assert main.wind_direction == "340"
 
 
-def test_pass_in_observation_with_33007G15KT_in_it():
-    assert main.find_wind_in_observation(
-        "METAR KJSO 121815Z AUTO 33007G15KT 10SM CLR 18/10 A3019 RMK AO2 T01910098=") == "33007G15KT"
+def test_find_wind_speed_in_observation():
+    main.find_wind_in_observation(
+        "METAR KJSO 121815Z AUTO 33007G15KT 10SM CLR 18/10 A3019 RMK AO2 T01910098=")
+
+    assert main.wind_speed == "07"
+
+
+def test_find_gust_speed_in_observation():
+    main.find_wind_in_observation(
+        "METAR KJSO 121815Z AUTO 33007G15KT 10SM CLR 18/10 A3019 RMK AO2 T01910098=")
+
+    assert main.gust_speed == "15"
+
+
+def test_find_no_gust_speed_in_observation():
+    main.find_wind_in_observation(
+        "METAR KJSO 121815Z AUTO 33007KT 10SM CLR 18/10 A3019 RMK AO2 T01910098=")
+
+    assert main.gust_speed == "None"
+
+
+def test_find_wind_unit_KT_in_observation():
+    main.find_wind_in_observation(
+        "METAR KJSO 121815Z AUTO 33007KT 10SM CLR 18/10 A3019 RMK AO2 T01910098=")
+
+    assert main.wind_unit == "KT"
+
+
+def test_find_wind_unit_MPS_in_observation():
+    main.find_wind_in_observation(
+        "METAR KJSO 121815Z AUTO 33007MPS 10SM CLR 18/10 A3019 RMK AO2 T01910098=")
+
+    assert main.wind_unit == "MPS"
+
+
+def test_find_wind_direction_VRB_in_observation():
+    main.find_wind_in_observation(
+        "METAR KNOG 121756Z AUTO VRB05KT 9SM FEW029 26/17 A3014 RMK AO2 SLP187 6//// T02610172 10261 20144 58006 PNO $=")
+
+    assert main.wind_direction == "VRB"
+
+
+def test_find_wind_direction_for_calm_wind():
+    main.find_wind_in_observation(
+        "METAR KTOA 121747Z 00000KT 10SM SKC 31/06 A3009=")
+
+    assert main.wind_direction == "000"
+
+
+def test_find_wind_speed_for_calm_wind():
+    main.find_wind_in_observation(
+        "METAR KTOA 121747Z 00000KT 10SM SKC 31/06 A3009=")
+
+    assert main.wind_speed == "00"
